a7be948f4f3fa2ac5e64ce8f3d623e29631ba18f
kisanths <skisanth1114@gmail.com>
2026-08-18 21:57:11 +0530

    4.2

---
+++ Task 04/calculator.js
@@ -0,0 +1,14 @@
+let a=20;
+let b=5;
+let op="+";
+if (op==="+"){
+    console.log(a+b);
+} else if (op==="-"){
+    console.log(a-b);
+} else if (op==="*"){
+    console.log(a*b);
+} else if (op ==="/"){
+    console.log(a/b);
+} else{
+    console.log("invalid");
+}
